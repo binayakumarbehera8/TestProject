cb67a7a4c8eac942b35ec112869aecde5f5f9a61
binay <binay@LAPTOP-HP5BHKPS>
2019-01-06 19:20:43 +0530

    First Application pushed to git

---
+++ MyWebApp/src/main/java/com/binaya/arisglobal/MyWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.binaya.arisglobal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebAppApplication.class, args);
+	}
+
+}
+
